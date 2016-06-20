60dec600a1cb53c27886f6e2926e094edd770519
Tushar <tusharm567@gmail.com>
2016-06-20 14:27:04 +0530

    Fixed File upload

--- client/components/add-listing/add-listing.js
+++ client/components/add-listing/add-listing.js
@@ -1,1 +1,6 @@
 Meteor.subscribe('listings');
+Uploader.finished = function(index, fileInfo, templateContext) {
+	console.log(fileInfo.uploadedName);
+	document.getElementById("uploadedImg").style.display = "block";
+	document.getElementById("uploadedImg").src = 'http://localhost:3000/upload/' + fileInfo.uploadedName;
+}
