7e63d8da32e70382490a5e9127f7bccf05a997ab
vkku <kumar.vivekten@gmail.com>
2016-07-05 22:09:54 +0530

    Added login status messages

--- client/components/login/index.js
+++ client/components/login/index.js
@@ -1,11 +1,16 @@
 Template.loginForm.events({
     'submit form': function(event)
-    {
+    {    	
+		var print = document.getElementById('sign-in-msg');
         event.preventDefault();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
         Meteor.loginWithPassword(email, password, function(error){
-    	console.log(error.reason);
+        	if(error)
+				print.innerHTML = error.reason;
+				//console.log(error.reason);
 		});
+
+				print.innerHTML = "Login Successfull";
     }
 });
