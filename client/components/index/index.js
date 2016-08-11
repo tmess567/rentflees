eb3ac6c509917afeb350214148b352fa62766041
Ajay Saini <saini.ajay172@gmail.com>
2016-08-12 02:37:46 +0530

    Fixed city image issues

--- client/components/index/index.js
+++ client/components/index/index.js
@@ -5,19 +5,4 @@ Template.index.onRendered(function(){
 	  var city = $(this).attr('data-city');
 	  $('#top-cities-div').toggleClass('bg-' + city);
 	});
-
-	$('.photo').slick({
-		dots: false,
-		arrows: false,
-		infinite: true,
-	  	speed: 500,
-	  	fade: true,
-	  	cssEase: 'linear',
-	  	slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 5000,
-		//centerMode: true,
-  		//variableWidth: true
-	});
 });
