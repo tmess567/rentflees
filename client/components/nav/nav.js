086fb8c42410d2933acf678d1132298bb0226a02
Tushar <tusharm567@gmail.com>
2016-08-12 00:56:53 +0530

    Fixed nav bar

--- client/components/nav/nav.js
+++ client/components/nav/nav.js
@@ -1,40 +1,7 @@
-var lastScrollTop = 0;
-$(document).on('scroll', function(){
-    var currentScrollTop = $(this).scrollTop();
-    if(currentScrollTop > lastScrollTop+5) {
-    	$('nav').addClass('hide');
-    }
-    else if(currentScrollTop < lastScrollTop-5){
-    	$('nav').removeClass('hide');
-    }
-    lastScrollTop = currentScrollTop;
-});
-Template.nav.helpers({
-	btnContent: function(){
-		if(this.isLoggedIn)
-			return 'Hello, '+ this.username();
-		else
-			return 'Sign In';
-	},
-    isLoggedIn: function(){
-    	if(Meteor.user)
-    		return true;
-    	return false;
-    },
-    username: function(){
-    	return Meteor.user.username;
-    }
-});
-
 Template.nav.events({
-	'click #nav-btn-accounts': function(){
-		if (Meteor.user() !== null) {
-			//user is logged in sign Out 
-			Meteor.logout();
-			$('#nav-btn-accounts').text("Sign In");
-		} else {
-			//user not logged in open sign in modal
-			$('#login-modal').modal();
-		}
-	}
+    'click div.sign-out': function(event)
+    {
+        event.preventDefault();
+        Meteor.logout();
+    }
 });
