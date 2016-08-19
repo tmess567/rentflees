8c28275dd10f327609c640de58b7ecc6f82391dd
Tushar <tusharm567@gmail.com>
2016-08-19 23:28:25 +0530

    Added City filters

--- client/components/mapview/mapview.js
+++ client/components/mapview/mapview.js
@@ -1,8 +1,34 @@
+var listingsIndexVar = null;
+var listingsIndexDep = new Tracker.Dependency();
+
 Template.searchBox.helpers({
-  listingsIndex: () => ListingsIndex
+  listingsIndex : function() {
+  	listingsIndexDep.depend();
+  	return listingsIndexVar===null?ListingsIndex:listingsIndexVar;
+  },
+  listingsArr : function() {
+  	listingsIndexDep.depend();
+  	return listingsIndexVar===null?ListingsIndex.search("").fetch():listingsIndexVar;
+  }
 });
+
+Template.searchBox.rendered = function() {
+	listingsIndexVar = ListingsIndex.search("").fetch();
+	listingsIndexDep.changed();
+};
+
 Template.searchBox.rendered = function(){
 	$('.easy-search-input-container > input').addClass('form-control');
 	$('.easy-search-input-container > input').addClass('search-input');
 	$('.easy-search-input-container > input').attr('placeholder', 'Search')
 };
+Template.searchBox.events({
+	"click .city-dropdown > li" : function(evt) {
+		$(".city-btn").html(evt.target.innerHTML + "<span class=\"caret\"></span>");
+		if(evt.target.innerHTML === "All Cities")
+			listingsIndexVar = ListingsIndex.search("").fetch();
+		else
+			listingsIndexVar = ListingsIndex.search(evt.target.innerHTML).fetch();
+		listingsIndexDep.changed();
+	}
+});
