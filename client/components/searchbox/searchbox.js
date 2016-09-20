d47d05494d6e96edbc56f3905c5b1467b5f7df34
Tushar <tusharm567@gmail.com>
2016-09-20 06:05:29 +0530

    Index AutoComplete

--- client/components/searchbox/searchbox.js
+++ client/components/searchbox/searchbox.js
@@ -3,8 +3,9 @@ Template.searchbox.onRendered(function(){
 		e.preventDefault();
 		var city = $(this).attr("href").replace("#","");
 		var cityName = $(this).text();
+		$('#autocomplete').val(city);
+		$('#autocomplete').focus();		
 		$('.searchbox span#city-span').text(cityName);
-		$('.searchbox #city').val(city);
   	});
 
   	$(".searchbox *").focus(function(e){
@@ -14,4 +15,56 @@ Template.searchbox.onRendered(function(){
 	    console.log("out");
 	    $(this).closest('.searchbox').removeClass("focus");
 	});
+
+	$("#autocomplete").focus(function(e){
+		var placeSearch, autocomplete;
+			initAutocomplete();
+	    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+	});
+	$(".go").click(function(e){
+		let query = $("#autocomplete").val();
+		if(query !== "")
+			window.location.href="/map?q="+query;
+	});
+	GoogleMaps.load({
+    v: '3',
+    key: 'AIzaSyBiTVrSTOhuNSaxTT29FqS1bsa3OXHhulc',
+    libraries: 'geometry,places'
+  });
+  
+/*GoogleMaps.ready('map', function(map) {
+	
+      
+});*/
+
 });
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        console.log("init");
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['geocode']});
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', gotoList());
+      }
+
+      function gotoList() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+        console.log(place);
+      }
